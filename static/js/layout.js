61eda3dc4278cf38e87cb11c01c75e69a5288b79
swe <swe@josue>
2019-03-27 03:19:48 -0400

    Added dynamic comment form via javascript and css

--- static/js/layout.js
+++ static/js/layout.js
@@ -66,7 +66,13 @@ nav_layout.controller('LayoutCtrl', ['$scope', '$mdSidenav', '$http', '$log', '$
 
 
   $scope.update_object = function() {
-    var url = document.getElementById("current_object_json").value
+    try {
+      var url = document.getElementById("current_object_json").value;
+    }
+    catch(e) {
+      $log.error("no value 'current_object_json' on page...");
+      return null;
+    }
     $http.get(url)
     .then(function(response) {
       $scope.object = response.data;
@@ -92,6 +98,46 @@ nav_layout.controller('LayoutCtrl', ['$scope', '$mdSidenav', '$http', '$log', '$
   $scope.get_search = function() {
     var url = "/ministry/search/" + $scope.query;
     $location.url(url);
+  };
+
+  /** Controls the display of new comment forms on the page.
+   *
+   *  All elements of 'new_comment' are styled (forms hidden, buttons shown).
+   *  Then the targeted comment form is shown and the button is hidden.
+   *  This ensures that only a single form is shown at a time.
+   *
+   *  TODO: comment form should be dynamically created instead of hardcoded
+   **/
+  $scope.unhide_new_comment = function(event) {
+
+    // reset all 'new_comment' divs to default styling
+    var new_comment_divs = document.getElementsByClassName('new_comment');
+    for (d = 0; d < new_comment_divs.length; d++) {
+      new_comment_divs[d].children[0].style.display = 'none';
+      new_comment_divs[d].children[1].style.display = 'block';
+    }
+
+    // style selected form and button respectively
+    if ($(event.target).parent().parent()[0].classList.value.includes("wrapper")) {
+      // clicked button and not inner text. therefore, dont unpack as many parent elements
+      $(event.target).parent().children().css('display', 'block');
+      $(event.target).css('display', 'none');
+    } else {
+      $(event.target).parent().parent().children().css('display', 'block');
+      $(event.target).parent().css('display', 'none');
+    };
+  };
+
+  $scope.hide_new_comment = function(event) {
+    // hide selected form and button respectively
+    if ($(event.target).parent().parent().parent()[0].classList.value.includes("wrapper")) {
+      // clicked button and not inner text. therefore, dont unpack as many parent elements
+      $(event.target).parent().parent().children().css('display', 'block');
+      $(event.target).parent().css('display', 'none');
+    } else {
+      $(event.target).parent().parent().parent().children().css('display', 'block');
+      $(event.target).parent().parent().css('display', 'none');
+    };
   };
 }]);
 
