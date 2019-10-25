12ca953c8d2755db7d78b376bb6655b07d440174
Josue David Figueroa <swe.fig@gmail.com>
2019-10-25 15:48:45 -0400

    Ported login functionality

--- people/urls.py
+++ people/urls.py
@@ -13,13 +13,6 @@ urlpatterns = [
     path('create', views.create_user, name='create_user'),
     path('profile', views.user_profile, name='user_profile'),
 
-    path('login/',
-         RedirectView.as_view(url='/#/people/login')),
-    path('new/',
-         RedirectView.as_view(url='/#/people/create')),
-    path('profile/',
-         RedirectView.as_view(url='/#/people/profile')),
-
     path('messages/json', views.messages_json),
     path('donations/json', views.donation_json),
 ]
