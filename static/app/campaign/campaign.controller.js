bc5ab297196742d0c2067b5cf28ef119fbb317a3
Josue David Figueroa <swe.fig@gmail.com>
2019-11-17 01:08:16 -0500

    Reimplemented like button for Campaign and MinistryProfile view pages via AngularJS

--- static/app/campaign/campaign.controller.js
+++ static/app/campaign/campaign.controller.js
@@ -2,17 +2,13 @@ angular.module('LON').controller('campaignCtrl', campaignCtrl);
 campaignCtrl.$inject = ['$scope', '$routeParams', '$timeout', 'objectService', 'likeButtonService', 'galleryService'];
 
 function campaignCtrl($scope, $routeParams, $timeout, objectService, likeButtonService, galleryService) {
-  // TODO: change title block
   $scope.object = objectService.get;
   $scope.likeButton = likeButtonService;
-
-  $scope.currentNavItem = 'Home';
 
   $scope.gallery = [];
 
   // activate dynamic content
   activate();
-  share_button();
 
   // activate dynamic content
   $scope.$on('$destroy', function() {
@@ -21,28 +17,15 @@ function campaignCtrl($scope, $routeParams, $timeout, objectService, likeButtonS
   });
 
   function activate() {
+    const campaign_id = document.getElementById('campaign_id').value;
     // get campaign content
     objectService.periodically_fetch();
 
     // get gallery content
-    const gallery_url = "/t/campaign/" + $routeParams.campaign_id + "/gallery/json";
+    const gallery_url = "/campaign/" + campaign_id + "/gallery/json";
     return galleryService.get(gallery_url)
     .then(function(data) {
       $scope.gallery = data;
     })
   }
-
-  function share_button() {
-    // hack to update share button with attributes from objectService because promises don't seem to be working
-    $scope.share.reset();
-
-    $timeout(update_dom, 250);
-
-    function update_dom() {
-      $scope.share.set('title', 'Check out this fundraiser for ' + $scope.object().name + ' on Love Our Neighbor!');
-      $scope.share.set('url', 'https://loveourneighbor.org' + $scope.object().url.substr(2));
-      $scope.share.update_dom();
-    }
-  }
-  ga('send', 'pageview', '/campaigns/' + $routeParams.campaign_id);
 }
