0599e4194768563316ee877639eef8febed98d77
Josue David Figueroa <swe.fig@gmail.com>
2019-12-28 16:55:37 -0500

    Fixed banner selection dialog trigger for campaign creation/edit

--- static/app/campaign/campaign_action.controller.js
+++ static/app/campaign/campaign_action.controller.js
@@ -4,28 +4,24 @@ campaignActionCtrl.$inject = ['$scope', 'tagService', 'objectService', 'selectIm
 function campaignActionCtrl($scope, tagService, objectService, selectImageService, confirmDeleteDialogService) {
   const campaign_id = document.getElementById('campaign_id');
 
+  $scope.banner_urls = {};
+  $scope.select_banner = select_banner;
+  $scope.banner_img_dialog = selectImageService;
+  $scope.selected_banner = $scope.banner_img_dialog.selected;
+
+  $scope.filter_tags = tagService.search;
+
+  $scope.tag_service = tagService;
+
   // creating a new campaign
   if (campaign_id === null) {
     $scope.object = {'tags': []};     // work around
     $scope.filter_tags = tagService.search;
-    $scope.tag_service = tagService;
     tagService.fetch();
   }
   // edit campaign
   else {
     $scope.object = objectService.get;
-    $scope.filter_tags = tagService.search;
-    $scope.tag_service = tagService;
-
-    $scope.banner_urls = {};
-    $scope.select_banner = select_banner;
-    $scope.banner_img_dialog = selectImageService;
-    $scope.selected_banner = $scope.banner_img_dialog.selected;
-
-    $scope.profile_img_urls = {};
-    $scope.select_profile_img = select_profile_img;
-    $scope.profile_img_dialog = selectImageService;
-    $scope.selected_profile_img = $scope.profile_img_dialog.selected;
 
     $scope.confirmDelete = confirmDeleteDialogService;
 
