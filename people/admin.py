5ff6925583e76abfef0bbc80b519705cddd2659d
Josue David Figueroa <swe.fig@gmail.com>
2020-02-07 00:57:12 -0500

    Show profile image in User admin page

--- people/admin.py
+++ people/admin.py
@@ -29,9 +29,9 @@ class UserAdmin(DjangoUserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'display_name')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'display_name', 'profile_img')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-         'groups', 'user_permissions')}),
+                                       'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
         # (_('Ids'), {'fields': ('private_uuid', 'public_id')}),
     )
