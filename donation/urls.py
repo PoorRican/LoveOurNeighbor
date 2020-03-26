0846310a7ac5a957676adb9136c790aa05dd71d2
Josue David Figueroa <swe.fig@gmail.com>
2020-03-26 17:09:00 -0400

    Remove unnecessary templates/views

--- donation/urls.py
+++ donation/urls.py
@@ -4,19 +4,6 @@ from . import views
 
 app_name = 'donation'
 urlpatterns = (
-    path('admin', views.admin_donation,
-         name='admin_donation'),
-
-    path('campaign/<int:campaign_id>/select', views.select_payment,
-         name='select_payment'),
-
-    path('<int:donation_id>/cc', views.cc_payment,
-         name='cc_payment'),
-    path('<int:donation_id>/other', views.braintree_payment,
-         name='braintree_payment'),
-    path('<int:donation_id>/btc', views.coinbase_payment,
-         name='coinbase_payment'),
-
     path('<int:donation_id>/complete', views.payment_complete,
          name='payment_complete'),
 
