a51e6f2ff2bc52ab92aa0b8d74a08b897e85a521
Josue David Figueroa <swe.fig@gmail.com>
2020-04-09 14:13:33 -0400

    Fixed error on ministry/campaign admin pages with no views/likes

--- ministry/admin.py
+++ ministry/admin.py
@@ -10,8 +10,8 @@ class MinistryProfileAdmin(ModelAdmin):
     }
     list_display = ('name', 'pub_date', 'verified', 'admin')
     search_fields = ('name', 'admin', 'tags__name')
-    readonly_fields = ('pub_date', 'likes', 'views')
-    fieldsets = (('Metadata', {'fields': ('pub_date', 'likes', 'views')}),
+    readonly_fields = ('pub_date',)
+    fieldsets = (('Metadata', {'fields': ('pub_date',)}),
                  ('Profile Data',
                   {'fields': (('name', 'verified'), 'description', 'tags', 'profile_img', 'banner_img')}),
                  ('Administration', {'fields': ('admin', 'reps')}),
